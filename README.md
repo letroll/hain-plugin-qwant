4d41065acd6ee251b4655998a1faadeb8ed2b939
jquievreux <jquievreux@cityway.fr>
2016-05-04 17:00:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# hain-plugin-qwant
+
+A simple [Hain](https://github.com/appetizermonster/hain) plugin for searching on Qwant.
+
+# Install
+Open Hain and type
+
+```
+/hpm install hain-plugin-qwant
+```
+
+# Usage
+```
+/qw something
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (C) 2016  Letroll
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+'use strict'
+
+module.exports = (pluginContext) => {
+  const shell = pluginContext.shell
+
+  function search (query, res) {
+    const query_trim = query.trim()
+
+    if (query_trim.length === 0) {
+      return
+    }
+
+    res.add({
+      id: query_trim,
+      payload: 'open',
+      title: query_trim,
+      desc: 'Search on Qwant.com'
+    })
+  }
+
+  function execute (id, payload) {
+    if (payload !== 'open') {
+      return
+    }
+    shell.openExternal(`http://www.qwant.com/?q=${id}`)
+  }
+
+  return { search, execute }
+}
